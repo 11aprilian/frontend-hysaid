0e18223ff59f788d5ffa257e4299a1db5ce84338
11aprilian <ihzaaprilian@gmail.com>
2024-01-12 18:49:32 +0700

    asasass

--- app/components/MobileNavbar.jsx
+++ app/components/MobileNavbar.jsx
@@ -33,13 +33,13 @@ const MobileNavbar = () => {
   };
   return (
     <div>
-      <div className=" fixed z-50 w-80 h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-lg bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
+      <div className=" fixed z-50 w-80 h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-lg bottom-4 left-1/2">
         <div className="grid h-full max-w-lg grid-cols-5 mx-auto">
           <Link
             href={"/"}
             data-tooltip-target="tooltip-home"
             type="button"
-            className="inline-flex flex-col items-center justify-center px-5 rounded-s-lg hover:bg-gray-50 dark:hover:bg-gray-800 group"
+            className="inline-flex flex-col items-center justify-center px-5 rounded-s-lg hover:bg-gray-50 group"
           >
             <IoHomeOutline />
             <span className="sr-only">Home</span>
@@ -47,7 +47,7 @@ const MobileNavbar = () => {
           <div
             id="tooltip-home"
             role="tooltip"
-            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip"
           >
             Home
             <div className="tooltip-arrow" data-popper-arrow></div>
@@ -55,7 +55,7 @@ const MobileNavbar = () => {
           <button
             data-tooltip-target="tooltip-wallet"
             type="button"
-            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 group"
           >
             <IoSearch />
             <span className="sr-only">Search</span>
@@ -63,7 +63,7 @@ const MobileNavbar = () => {
           <div
             id="tooltip-wallet"
             role="tooltip"
-            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip"
           >
             Search
             <div className="tooltip-arrow" data-popper-arrow></div>
@@ -73,7 +73,7 @@ const MobileNavbar = () => {
               href={"/createpost"}
               data-tooltip-target="tooltip-new"
               type="button"
-              className="inline-flex items-center justify-center w-10 h-10 font-medium bg-emerald-400 rounded-full hover:bg-emerald-300 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
+              className="inline-flex items-center justify-center w-10 h-10 font-medium bg-emerald-400 rounded-full hover:bg-emerald-300 group focus:ring-4 focus:ring-blue-300 focus:outline-none"
             >
               <IoCreateOutline />
               <span className="sr-only">Post</span>
@@ -82,7 +82,7 @@ const MobileNavbar = () => {
           <div
             id="tooltip-new"
             role="tooltip"
-            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip "
           >
             Post
             <div className="tooltip-arrow" data-popper-arrow></div>
@@ -91,7 +91,7 @@ const MobileNavbar = () => {
             href={"/profile"}
             data-tooltip-target="tooltip-settings"
             type="button"
-            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 group"
           >
             <IoPersonOutline />
             <span className="sr-only">Profile</span>
@@ -99,7 +99,7 @@ const MobileNavbar = () => {
           <div
             id="tooltip-settings"
             role="tooltip"
-            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip"
           >
             Profile
             <div className="tooltip-arrow" data-popper-arrow></div>
@@ -108,7 +108,7 @@ const MobileNavbar = () => {
             onClick={handleLogout}
             data-tooltip-target="tooltip-profile"
             type="button"
-            className="inline-flex flex-col items-center justify-center px-5 rounded-e-lg hover:bg-gray-50 dark:hover:bg-gray-800 group"
+            className="inline-flex flex-col items-center justify-center px-5 rounded-e-lg hover:bg-gray-50 group"
           >
             <IoLogOutOutline />
             <span className="sr-only">Logout</span>
@@ -116,7 +116,7 @@ const MobileNavbar = () => {
           <div
             id="tooltip-profile"
             role="tooltip"
-            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip"
           >
             Logout
             <div className="tooltip-arrow" data-popper-arrow></div>
